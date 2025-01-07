eb79a50f0a7aa1e13e0679f3b0a57d530346a84a
thallysfs <thallys@hotmail.com>
2025-01-06 21:34:48 -0300

    first commit

---
+++ src/app/(rs)/template.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default async function Template({
+    children,
+}: {
+    children: React.ReactNode
+}) {
+    return (
+        <div className="animate-appear">
+            {children}
+        </div>
+    )
+}
