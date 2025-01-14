bf20bcceca63a6ff95f6a9bd9ea1e20f2a462d3f
thallysfs <thallys@hotmail.com>
2025-01-13 22:04:14 -0300

    feat: add config tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,11 +3,11 @@ import type { Config } from "tailwindcss";
 export default {
     darkMode: ["class"],
     content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
+		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+	theme: {
   	extend: {
 		backgroundImage: {
 			'home-img': "url('/images/home-img.jpg')"
@@ -58,7 +58,29 @@ export default {
   			lg: 'var(--radius)',
   			md: 'calc(var(--radius) - 2px)',
   			sm: 'calc(var(--radius) - 4px)'
-  		}
+  		},
+		keyframes: {
+			"appear": {
+				from: { 
+					opacity: "0",
+				},
+				to: {
+					opacity: "1"
+				} 
+			},
+			"slide": {
+				from: {
+					"transform": "translateX(100%)",
+				},
+				to: {
+					"transform": "translateX(0%)",
+				}
+			}
+		},
+		animation:{
+			"appear": "appear 1s ease-in-out",
+			"slide": "slide 750ms ease-in-out"
+		}
   	}
   },
   plugins: [require("tailwindcss-animate")],
